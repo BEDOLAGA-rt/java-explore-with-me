ae19eb996d789d3178e6377b8cf1cd64229bd919
arsenijprokofev <arseniyprokofev17279@gmail.com>
2026-02-28 01:13:10 +0300

    2 этап

--- stats/stats-server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.stats.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,18 @@ public class ErrorHandler {
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, Object> handleBadRequest(BadRequestException e) {
+        log.error("400: {}", e.getMessage());
+        return Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", 400,
+                "error", "Bad Request",
+                "message", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleMissingParams(MissingServletRequestParameterException e) {
         log.error("400: {}", e.getMessage());
         return Map.of(
                 "timestamp", LocalDateTime.now(),
